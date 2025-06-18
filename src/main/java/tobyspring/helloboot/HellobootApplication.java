c055c324405b9420fa62b2f470f3782a280f93fe
swkimm <swkim7161@kakao.com>
2025-06-18 12:01:11 +0900

    Annotation Component Scan

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,23 +4,14 @@ package tobyspring.helloboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
+@ComponentScan
 public class HellobootApplication {
-	// 자바 팩토리 메소드 만들기
-	@Bean
-	public HelloController helloController(HelloService helloService) {
-		return new HelloController(helloService);
-	}
-
-	@Bean
-	public HelloService helloService() {
-		return new SimpleHelloService();
-	}
 
 	public static void main(String[] args) {
 		// 스프링 컨테이너 만들기
