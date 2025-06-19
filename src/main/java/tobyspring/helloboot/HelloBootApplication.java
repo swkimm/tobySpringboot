78f9bddfafd468c8b279f0a958366fe889b5001d
swkimm <swkim7161@kakao.com>
2025-06-19 13:53:54 +0900

    인프라 빈 구성 정보의 분리

---
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -0,0 +1,14 @@
+package tobyspring.helloboot;
+
+
+import org.springframework.boot.SpringApplication;
+import tobyspring.config.MySpringBootApplication;
+
+@MySpringBootApplication
+public class HelloBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloBootApplication.class, args);
+	}
+
+}
